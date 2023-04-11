0bca73fac340abae45b9ec0b93db1e14f56cd0cc
Andreas Vrachimi <avrachimi@hotmail.com>
2023-04-11 17:50:53 +0100

    Responsive: Single Movie Search page

--- src/pages/search/movies/[id].tsx
+++ src/pages/search/movies/[id].tsx
@@ -72,44 +72,51 @@ const SingleSearchMovie = () => {
         <Navbar />
         <div className="flex flex-col items-center justify-center">
           <div className="my-2 text-3xl">{movie.Title}</div>
-          <img
-            src={movie.Poster}
-            className="my-3 w-[65%] border object-contain"
-          />
-          <div className="text-md my-10 px-4 text-center">{movie.Plot}</div>
-          <div className="my-2 flex w-full flex-col items-center justify-center text-center">
-            {!isMovieInDB && (
-              <button
-                className="mt-10 rounded-md border-2 bg-green-800 p-2 text-gray-200"
-                onClick={markWatched}
-              >
-                Add to database
-              </button>
-            )}
-            {isMovieInDB && !dbMovie && (
-              <div className="mb-10 w-[80%] rounded-md border-2 border-red-900 bg-red-800 bg-opacity-70 px-2">
-                Movie already added to database.
-                <div>
-                  Visit{" "}
-                  <Link href={`/`} className="underline">
-                    Home
-                  </Link>{" "}
-                  to find movies in our database.
-                </div>
+          <div className="flex flex-col items-center justify-center md:my-5 md:w-[70%] md:flex-row">
+            <img
+              src={movie.Poster}
+              className="my-3 w-[65%] border object-contain md:w-full xl:w-[60%]"
+            />
+            <div className="flex flex-col items-center justify-center">
+              <div className="text-md my-10 px-4 text-center">{movie.Plot}</div>
+              <div className="my-2 flex w-full flex-col items-center justify-center text-center">
+                {!isMovieInDB && (
+                  <button
+                    className="mt-10 rounded-md border-2 bg-green-800 p-2 text-gray-200"
+                    onClick={markWatched}
+                  >
+                    Add to database
+                  </button>
+                )}
+                {isMovieInDB && !dbMovie && (
+                  <div className="mb-10 w-[80%] rounded-md border-2 border-red-900 bg-red-800 bg-opacity-70 px-2">
+                    Movie already added to database.
+                    <div>
+                      Visit{" "}
+                      <Link href={`/`} className="underline">
+                        Home
+                      </Link>{" "}
+                      to find movies in our database.
+                    </div>
+                  </div>
+                )}
+                {isMovieInDB && dbMovie && (
+                  <div className="mb-10 w-[80%] rounded-md border-2 border-red-900 bg-red-800 bg-opacity-70 px-2">
+                    Movie already added to database.
+                    <div>
+                      Click{" "}
+                      <Link
+                        href={`/movies/${dbMovie.id}`}
+                        className="underline"
+                      >
+                        here
+                      </Link>{" "}
+                      to visit movie page.
+                    </div>
+                  </div>
+                )}
               </div>
-            )}
-            {isMovieInDB && dbMovie && (
-              <div className="mb-10 w-[80%] rounded-md border-2 border-red-900 bg-red-800 bg-opacity-70 px-2">
-                Movie already added to database.
-                <div>
-                  Click{" "}
-                  <Link href={`/movies/${dbMovie.id}`} className="underline">
-                    here
-                  </Link>{" "}
-                  to visit movie page.
-                </div>
-              </div>
-            )}
+            </div>
           </div>
         </div>
       </main>
