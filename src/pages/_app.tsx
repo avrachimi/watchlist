940ac7d30a330eada20c0d6cc0ed5249bfa1b11d
Andreas Vrachimi <avrachimi@hotmail.com>
2023-04-13 15:22:24 +0100

    tried to fix redirect issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} className=" overflow-y-scroll" />
+      <Component {...pageProps} className="overflow-y-scroll" />
     </SessionProvider>
   );
 };
